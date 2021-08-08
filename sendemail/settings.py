66914326a54bc611ade1865f806ef262905251ee
Shakil Ahmed Khan <shakil.gd13@gmail.com>
2021-08-08 20:13:10 +0600

    Update settings.py
    Updated security

--- sendemail/settings.py
+++ sendemail/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j+1s-m_wup-25fttm_3vqi1xr0=ld9qkvn(*6dv%rkoa*0_t)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
